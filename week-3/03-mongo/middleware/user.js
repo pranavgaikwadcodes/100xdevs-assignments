5afdb5d31b22bdd4913a4b8e2bd542af2a474b18
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-10-16 20:39:47 +0530

    .

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,22 @@
+const { User } = require("../db");
+
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    User.findOne({
+        username: username,
+        password: password,
+    })
+    .then( (user) => {
+        if(user) {
+            next()
+        } else {
+            return res.status(403).json({msg: "invalid user credentials"})
+        }
+    } )
 }
 
 module.exports = userMiddleware;
--- week-3/03-mongo/routes/admin.js
+++ week-3/03-mongo/routes/admin.js
@@ -1,18 +1,44 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    try {
+        const checkUserExist = await Admin.findOne({username})
+        if(checkUserExist) {
+            return res.status(400).send({message: "Username already exists"})
+        }
+
+        await Admin.create({ username, password });
+        res.status(201).send({ message: "Admin created successfully" });
+    } catch (error) {
+        console.error(err);
+        res.status(500).send({ message: "Error creating admin" });
+    }
 });
 
-router.post('/courses', adminMiddleware, (req, res) => {
+router.post('/courses', adminMiddleware, async (req, res) => {
     // Implement course creation logic
+    const title = req.body.title;
+    const description = req.body.description;
+    const imageLink = req.body.imageLink;
+    const price = req.body.price;
+
+    const course = await Course.create({ title, description, imageLink, price });
+    res.status(200).json({ message: "Course created successfully", courseId: course._id })
+
 });
 
-router.get('/courses', adminMiddleware, (req, res) => {
+router.get('/courses', adminMiddleware, async (req, res) => {
     // Implement fetching all courses logic
+    const allCourses = await Course.find({});
+    res.status(200).json({ message: "Courses fetched successfully", courses: allCourses })
 });
 
 module.exports = router;
