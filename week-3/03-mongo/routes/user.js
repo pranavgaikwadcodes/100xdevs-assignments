0b991ba6dab6291fc92978b242286d525f4a7288
Pranav Gaikwad <74352611+pranavgaikwadcodes@users.noreply.github.com>
2024-10-16 21:43:14 +0530

    .

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,18 +1,51 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User, Course } = require("../db");
+const { default: mongoose } = require("mongoose");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+    try {
+        const checkUserExist = await User.findOne({ username })
+        if (checkUserExist) {
+            return res.status(400).send({ message: "Username already exists" })
+        }
+
+        await User.create({ username, password });
+        res.status(201).send({ message: "User created successfully" });
+    } catch (error) {
+        console.error(err);
+        res.status(500).send({ message: "Error creating user" });
+    }
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+    const allCourses = await Course.find({});
+    res.status(200).json({ courses: allCourses })
 });
 
 router.post('/courses/:courseId', userMiddleware, (req, res) => {
     // Implement course purchase logic
+    const courseId = req.params.courseId;
+    const username = req.headers.username;
+
+    User.updateOne(
+        { username: username },
+        { "$push": { purchasedCourses: new mongoose.Types.ObjectId(courseId) } }
+    )
+        .catch((e) => {
+            console.error(e);
+        })
+        .then( (response) => {
+            res.status(200).json({ message: "Course purchased successfully" });
+        })
+
+    
 });
 
 router.get('/purchasedCourses', userMiddleware, (req, res) => {
